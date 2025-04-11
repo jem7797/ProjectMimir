0e5796a61cd6fb08260a84a393db4c8769c47b69
Jordan Molina <jordan.e.molina21@gmail.com>
2025-04-11 16:34:44 -0400

    removed uncessary logs

--- src/Profile Components/SideMenu.tsx
+++ src/Profile Components/SideMenu.tsx
@@ -1,47 +1,40 @@
-import { Sidebar, Menu, MenuItem, sidebarClasses } from 'react-pro-sidebar';
-import { Link } from 'react-router-dom';
-import { useSettings } from '../Components/Context/SettingContext';
-import SettingsMenu from '../Components/SettingsMenu';
+import { Sidebar, Menu, MenuItem, sidebarClasses } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
+import { useSettings } from "../Components/Context/SettingContext";
+import SettingsMenu from "../Components/SettingsMenu";
 
 const SideMenu = () => {
-const {settingsIsOpen, setSettingsIsOpen} = useSettings();
+  const { settingsIsOpen, setSettingsIsOpen } = useSettings();
 
-
-const handleClick = () => {
-setSettingsIsOpen(!settingsIsOpen);
-    console.log("settings");
+  const handleClick = () => {
+    setSettingsIsOpen(!settingsIsOpen);
   };
 
-
-
-
-
   return (
-<>
+    <>
+      {settingsIsOpen ? <SettingsMenu /> : ""}
 
-{settingsIsOpen ? <SettingsMenu/> : ""}
-
-
-    <Sidebar rootStyles={{
-        [`.${sidebarClasses.container}`]: {
-          backgroundColor: '#171929',
-          height: "100vh",
-          width:"250px",
-          padding: 0,
-          margin: 0,
-          overflow:"hidden",
-          whiteSpace:"none",
-          color: "white"
-        },
-      }} >
-       <Menu style={{padding: "10px"}}>
-    <MenuItem component={<Link to= "/"/>}> Mimir </MenuItem>
-    <MenuItem onClick={() => handleClick()} > Settings </MenuItem>
-  </Menu>
-    </Sidebar>
+      <Sidebar
+        rootStyles={{
+          [`.${sidebarClasses.container}`]: {
+            backgroundColor: "#171929",
+            height: "100vh",
+            width: "250px",
+            padding: 0,
+            margin: 0,
+            overflow: "hidden",
+            whiteSpace: "none",
+            color: "white",
+          },
+        }}
+      >
+        <Menu style={{ padding: "10px" }}>
+          <MenuItem component={<Link to="/" />}> Mimir </MenuItem>
+          <MenuItem onClick={() => handleClick()}> Settings </MenuItem>
+        </Menu>
+      </Sidebar>
     </>
-   
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
