3dad82325b4bd350b60a7767c9eb703da11f358e
Jordan Molina <jordan.e.molina21@gmail.com>
2025-05-25 23:46:33 -0400

    fixed formatting on Profile page

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,16 +1,14 @@
-import {  Flex, IconButton} from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
 import MainProfilePicture from "./Profile Components/MainProfilePicture";
 import ProfileInfo from "./Profile Components/ProfileInfo";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 
-
 const ProfileContent = () => {
   return (
-    <Flex height="100vh">
-      {/* Sidebar */}
-
-      <Link to={"/"}>
+    <Box >
+{/* back button */}
+      <Link to={"/"} >
         <IconButton
           aria-label={"back button"}
           icon={<IoMdArrowRoundBack />}
@@ -18,25 +16,20 @@ const ProfileContent = () => {
           fontSize={"xx-large"}
           marginTop={"5px"}
           marginLeft={"5px"}
+          height={"20px"}
           overflow={"hidden"}
         />
       </Link>
 
       {/* Main Content */}
-      <Flex
-        flex="1"
-        direction="column"
-        justify="center"
-        marginTop={"-320px"}
-        marginLeft={"-50"}
-      >
+      <Box flex="1" justifyContent={"center"} alignContent={""}>
         {/* Profile Picture */}
         <MainProfilePicture />
 
         {/* Profile Info */}
         <ProfileInfo />
-      </Flex>
-    </Flex>
+      </Box>
+    </Box>
   );
 };
 export default ProfileContent;
