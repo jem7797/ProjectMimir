8ee9286e334016d9d6726825fc1a772eeca2b412
Jordan Molina <jordan.e.molina21@gmail.com>
2025-05-28 00:21:32 -0400

    added tooltip to slider

--- src/Pages/Settings Components/Slider.tsx
+++ src/Pages/Settings Components/Slider.tsx
@@ -1,13 +1,17 @@
 import {
   Box,
+  HStack,
+  IconButton,
   Slider,
   SliderFilledTrack,
   SliderMark,
   SliderThumb,
   SliderTrack,
   Text,
+  Tooltip,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { FaRegCircleQuestion } from "react-icons/fa6";
 
 interface Props {
   sliderName: string;
@@ -15,17 +19,36 @@ interface Props {
 
 const SummarySlider = (sliderName: Props) => {
   const [sliderValue, setSliderValue] = useState(50);
+
   return (
-    <Box marginTop={"40"}>
-      <Text fontSize={"large"} alignContent={"flex-start"}>
-        {" "}
-        {sliderName.sliderName} Response Length:{" "}
-      </Text>
+    <Box>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        alignContent={"center"}
+        mt={40}
+      >
+        <HStack margin={"auto"}>
+          <Text fontSize={"large"} alignContent={"flex-start"}>
+            {" "}
+            Set {sliderName.sliderName} Length:{" "}
+            <Tooltip label="Controls Mimir’s response length: lower = shorter, higher = longer">
+              <IconButton
+                aria-label={"Info"}
+                icon={<FaRegCircleQuestion />}
+                variant={"ghost"}
+              />
+            </Tooltip>
+          </Text>
+        </HStack>
+      </Box>
       <Slider
         width={"60%"}
         marginTop={"10"}
         variant={"solid"}
         onChange={(val) => setSliderValue(val)}
+        step={10}
+        min={10}
       >
         <SliderTrack backgroundColor={"darkgrey"}>
           <SliderFilledTrack />
