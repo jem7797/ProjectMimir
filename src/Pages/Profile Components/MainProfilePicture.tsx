ceda935468ed18aa483d573aeff5bda191a84d87
Jordan Molina <jordan.e.molina21@gmail.com>
2025-05-27 20:25:46 -0400

    fixed profile picture

--- src/Pages/Profile Components/MainProfilePicture.tsx
+++ src/Pages/Profile Components/MainProfilePicture.tsx
@@ -1,15 +1,24 @@
-import {  Center, Icon, Image } from "@chakra-ui/react";
+import { Center, Icon, Image } from "@chakra-ui/react";
 import { SiTheodinproject } from "react-icons/si";
 import { useSignedIn } from "../../Components/Context/SignedInContext";
 
 const MainProfilePicture = () => {
-  const {signedInUser} = useSignedIn();
+  const { signedInUser } = useSignedIn();
   return (
     <Center>
-      <Icon boxSize={200}>
-        {signedInUser && signedInUser[2] ? <Image src={signedInUser[2]} boxSize={200} borderRadius="full" /> : <SiTheodinproject />}
-        
-      </Icon>
+      {signedInUser && signedInUser[2] ? (
+        <Image
+          src={signedInUser[2]}
+          boxSize={200}
+          borderRadius="full"
+          objectFit={"cover"}
+        />
+      ) : (
+        <Icon boxSize={200}>
+           <SiTheodinproject />
+        </Icon>
+      )}
+      
     </Center>
   );
 };
